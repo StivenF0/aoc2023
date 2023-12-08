677de4f338625c3fb9ef49b1aba8ab75fd9b8cbf
StivenF0 <stivenfonseca201@gmail.com>
2023-12-08 00:16:39 -0300

    Added the solution in python for day 1.

---
+++ day1/python/src/day1p1.py
@@ -0,0 +1,24 @@
+# Get lines from the input file
+lines = []
+with open("../../input.txt", "r") as file:
+    lines = [line.strip("\n") for line in file.readlines()]
+
+# Defining a list of possible numbers
+numbers_list = [str(i) for i in range(1,10)]
+
+# Get the numbers from the lines
+filtered_lines = []
+for line in lines:
+    filtered_line = []
+    for char in line:
+        if char in numbers_list:
+            filtered_line.append(char)
+    filtered_lines.append(filtered_line)
+
+# Filtering the first and last number for each line
+numbers = []
+for line in filtered_lines:
+    numbers.append(int(line[0] + line[-1]))
+
+# Get the sum of the numbers
+print(sum(numbers))
---
+++ day1/python/src/day1p2.py
@@ -0,0 +1,41 @@
+# Get lines from the input file
+lines = []
+with open("../../input.txt", "r") as file:
+    lines = [line.strip("\n") for line in file.readlines()]
+
+# Defining a dictionary of numbers
+numbers_dict = {
+    "1": "1", "one": "1",
+    "2": "2", "two": "2",
+    "3": "3", "three": "3",
+    "4": "4", "four": "4",
+    "5": "5", "five": "5",
+    "6": "6", "six": "6",
+    "7": "7", "seven": "7",
+    "8": "8", "eight": "8",
+    "9": "9", "nine": "9",
+}
+
+filtered_lines = []
+for line in lines:
+    # Get all intervals possible in the lines
+    intervals = []
+    for start in range(len(line)):
+        for end in range(len(line)):
+            intervals.append(line[start:end+1])
+    intervals = list(filter(lambda inter: inter != "", intervals))
+
+    # Get the "numbers" from the lines
+    filtered_line = []
+    for interval in intervals:
+        if interval in numbers_dict.keys():
+            filtered_line.append(interval)
+    filtered_lines.append(filtered_line)
+
+# Filtering to get just the first and last number corresponding to the dictionary
+numbers = []
+for line in filtered_lines:
+    numbers.append(int(numbers_dict[line[0]] + numbers_dict[line[-1]]))
+
+# Print the sum of the numbers
+print(sum(numbers))
