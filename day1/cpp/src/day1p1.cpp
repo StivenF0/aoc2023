76fe0bd687ad3ee56d485aebbb455b95515f1b3f
StivenF0 <stivenfonseca201@gmail.com>
2023-12-08 14:15:32 -0300

    Refactoring c++ code.

--- day1/cpp/src/day1p1.cpp
+++ day1/cpp/src/day1p1.cpp
@@ -1,65 +1,57 @@
-#include <fstream>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
-using namespace std;
+bool contains(std::vector<char> numbersList, char character) {
+  return (std::find(numbersList.begin(), numbersList.end(), character) != numbersList.end()) ? true : false;
+}
 
-bool isNumeric(char entry) {
-  return isdigit(entry);
+int concatConvert(char c1, char c2) {
+  std::string s1, s2;
+  s1 = std::string(1, c1);
+  s2 = std::string(1, c2);
+  return stoi(s1 + s2);
 }
 
 int main() {
-  // Getting all the lines on the file
-  string line;
-  ifstream input("input.txt");
-  vector<string> lines;
-  while (getline(input, line)) {
+  std::vector<std::string> lines;
+  std::fstream input("../../input.txt");
+  std::string line;
+  while(std::getline(input, line)) {
     lines.push_back(line);
   }
   input.close();
 
-  // Getting the numbers
-  vector<string> numbers;
-  for (string line : lines) {
-    char numbersOnLine[] = {'0', '0'};
-    int idx1 = 0;
-    bool foundSecond = false;
-    
-    for (char c : line) {
-      if (!isNumeric(c)) {
-        idx1++;
-        continue;
-      }
-      numbersOnLine[0] = c;
-      break;
-    }
-
-    int lineSize = line.size();
-    for (int i = line.size() - 1; i > 0; i--) {
-      if (!isNumeric(line[i]) || i == idx1) {
-        continue;
-      }
-
-      numbersOnLine[1] = line[i];
-      foundSecond = true;
-      break;
-    }
-
-    string number;
-    number += numbersOnLine[0];
-    number += foundSecond ? numbersOnLine[1] : numbersOnLine[0];
-    
-    numbers.push_back(number);
+  std::vector<char> numbersList;
+  for (int i = 1; i <= 9; i++) {
+    numbersList.push_back((char) i + '0');
   }
 
-  // Doing the sum
+  std::vector<std::vector<char>> filteredLines;
+  std::vector<char> filteredLine;
+  for (std::string line : lines) {
+    filteredLine.clear();
+    for (char character : line) {
+      if (contains(numbersList, character)){
+        filteredLine.push_back(character);
+      }
+    }
+    filteredLines.push_back(filteredLine);
+  }
+
+  std::vector<int> numbers;
+  for (std::vector<char> line : filteredLines) {
+    numbers.push_back(concatConvert(line.front(), line.back()));
+  }
+
   int sum = 0;
-  for (string num : numbers) {
-    sum += stoi(num);
+  for (int num : numbers) {
+    sum += num;
   }
 
-  cout << "Sum: " << sum << endl;
-
+  std::cout << "Sum: " << sum << '\n';
+  
   return 0;
 }
