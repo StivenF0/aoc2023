be460caf5d65be88b984b534e24d111220c010d8
StivenF0 <stivenfonseca201@gmail.com>
2023-12-09 14:33:01 -0300

    Made java version of day2

---
+++ day2/java/Day2p2.java
@@ -0,0 +1,71 @@
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.File;
+import java.io.FileNotFoundException; 
+import java.util.Scanner;
+
+public class Day2p2 {
+  static int getCubes(String cubeStr) {
+    String trimmedStr = cubeStr.trim();
+    String[] stringParts = trimmedStr.split(" ");
+    int cubes = Integer.parseInt(stringParts[0]);
+    return cubes;
+  }
+
+  public static void main(String[] args) {
+    final Map<String, Integer> MAX = new HashMap<>();
+    MAX.put("red", 12);
+    MAX.put("green", 13);
+    MAX.put("blue", 14);
+
+    ArrayList<String> lines = new ArrayList<>();
+    try {
+      File inputFile = new File("../input.txt");
+      Scanner inputScanner = new Scanner(inputFile);
+      while (inputScanner.hasNextLine()) {
+        lines.add(inputScanner.nextLine().trim());
+      }
+      inputScanner.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+
+    ArrayList<Map<String, Integer>> games = new ArrayList<>();
+    for (String line : lines) {
+      String[] gameRounds = line.split(":")[1].split(";");
+      int redMax = 0;
+      int greenMax = 0;
+      int blueMax = 0;
+      for (String gameRound : gameRounds) {
+        String[] cubes = gameRound.split(",");
+        for (String cubeRound : cubes) {
+          if (cubeRound.contains("red")) {
+            redMax = getCubes(cubeRound) > redMax ? getCubes(cubeRound) : redMax;
+          }
+          if (cubeRound.contains("green")) {
+            greenMax = getCubes(cubeRound) > greenMax ? getCubes(cubeRound) : greenMax;
+          }
+          if (cubeRound.contains("blue")) {
+            blueMax = getCubes(cubeRound) > blueMax ? getCubes(cubeRound) : blueMax;
+          }
+        }
+      }
+
+      Map<String, Integer> game = new HashMap<>();
+      game.put("red", redMax);
+      game.put("green", greenMax);
+      game.put("blue", blueMax);
+      
+      games.add(game);
+    }
+
+    int powerSum = 0;
+    for (Map<String, Integer> game : games) {
+      powerSum += game.get("red") * game.get("green") * game.get("blue");
+    }
+
+    System.out.println(powerSum);
+  }
+}
